2c43f2b77fc06c0d319dce7c1a274e5dd1382198
Your Name <you@example.com>
2017-03-18 15:53:33 +0800

    one

---
+++ Application/Api/Controller/UserController.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    public function index(){
+        $result = array();
+        $user_id = $_POST['user_id'];
+        $user_password = $_POST['user_password'];
+        $re = M('user')->where(array('user_id'=>$user_id))->find();
+        if (!isset($re)) {
+        	$state = 1;  //账号不存在
+        }else{
+        	$pd=M('user')->where(array('user_id'=>$user_id))
+        				 ->field('user_password')
+        				 ->find();
+        		if ($pd == $user_password) {
+        				 	$state = 3;  //密码正确，可以登录
+        				 }else{
+        				 	$state = 2;  //密码不正确
+        				 }
+        }
+        $result['status'] = $state;
+        echo json_encode($result);
+	}
+}
