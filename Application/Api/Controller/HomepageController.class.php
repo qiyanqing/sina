417e552658964e00812cf2baeaaf9d9471c78652
Your Name <you@example.com>
2017-03-26 22:30:59 +0800

    api

--- Application/Api/Controller/HomepageController.class.php
+++ Application/Api/Controller/HomepageController.class.php
@@ -7,16 +7,20 @@ class HomepageController extends Controller {
         $user_login = $_POST['user_id'];
         $user_password = $_POST['user_password'];
         $re = M('user')->where(array('user_login'=>$user_login))->find();
-        if (!isset($re)) {
-        	$state = 1;  //账号不存在
-        }else{
-        	$pd=M('user')->where(array('user_id'=>$user_id))
+        if (!$user_login) {
+           $state = 1;  //账号未输入！
+        }elseif(!isset($re)) {
+        	$state = 2;  //账号不存在!
+        }elseif(isset($re){
+        	$pd=M('user')->where(array('user_login'=>$user_login))
         				 ->field('user_password')
         				 ->find();
-        		if ($pd == $user_password) {
-        				 	$state = 3;  //密码正确，可以登录
-        				 }else{
-        				 	$state = 2;  //密码不正确
+        		if (!$user_password) {
+        				 	$state = 3;  //密码未输入！
+        				 }elseif($pd ！== $user_password){
+                            $state = 4;  //密码错误！
+                         }elseif($pd == $user_password){
+        				 	$state = 5;  //账号密码匹配，登陆成功！
         				 }
         }
         $result['status'] = $state;
