7c40db1862f067c1fa7f566cba48183727739157
zsyxydz <511639496@qq.com>
2017-03-26 22:43:31 +0800

    修改

--- Public/Home/homepage/js/sina_login.js
+++ Public/Home/homepage/js/sina_login.js
@@ -14,20 +14,49 @@ $(function(){
 	    });
 	    $(".btn_login").click(function(){
 	    	var ID=$(".accounts_login").val()
-	    	var password=$(".accounts_password").val()
-	    	console.log(ID,password)
+	    	var password=$(".accounts_password").val() 
 		    $.ajax({
-		    	url:"https://sina.wb/index.php/Api/Homepage/handlelogin",
+		    	url:"/index.php/Api/Homepage/handlelogin",
 		    	type:"post",
 		    	data:{
 		    		user_id:ID,
 		    		user_password:password
 		    	},
 		    	dataType:"json",
-		    	success:function res(){
+		    	success:function(res){
 		    		console.log(res)
+		    		var num=res.status
+		    		console.log(num)
+		    		if (num==1) {alert("请输入账号")}
+		    		if (num==2) {alert("账号不存在")}
+		    		if (num==3) {alert("请输入密码")}
+		    		if (num==4) {alert("密码错误")}
+		    		if (num==5) {alert("登陆成功")}
 		    	}
-		    })
+		    })  
 	    })
-	    
+	// 左侧选中样式
+	$(".webMain_left ul li").mouseover(function(){
+		$(this).addClass("select")
+	})
+	$(".webMain_left ul li").mouseout(function(){
+		$(this).removeClass("select")
+	})
+	$(".the_one").mouseout(function(){
+		$(this).addClass("select")
+	})
+	// 吸顶选项样式
+	$(".webTop_center .Web_top_nav ul li a,.webTop_center .Web_top_nav ul li").mouseover(function(){
+		$(this).css("color","#eb7350")
+	})
+	$(".webTop_center .Web_top_nav ul li a,.webTop_center .Web_top_nav ul li").mouseout(function(){
+		$(this).css("color","#808080")
+	})
+	// 登录样式
+	$(".btn_login").mouseover(function(){
+		$(this).css("background-color","#f77c3d")
+	})
+	$(".btn_login").mouseout(function(){
+		$(this).css("background-color","#ff8140")
+	})
 })
