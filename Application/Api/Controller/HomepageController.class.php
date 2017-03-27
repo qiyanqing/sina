c510f7bb90cf527bb3026dba6eebce6d726eb817
Your Name <you@example.com>
2017-03-27 16:49:00 +0800

    12345

--- Application/Api/Controller/HomepageController.class.php
+++ Application/Api/Controller/HomepageController.class.php
@@ -10,18 +10,28 @@ class HomepageController extends Controller {
         if (!$user_login) {
            $state = 1;  //账号未输入！
         }elseif(!isset($re)) {
-        	$state = 2;  //账号不存在!
-        }elseif(isset($re){
+            $state = 2;  //账号不存在!
+        
+        }elseif(isset($re)){
         	$pd=M('user')->where(array('user_login'=>$user_login))
         				 ->field('user_password')
         				 ->find();
-        		if (!$user_password) {
-        				 	$state = 3;  //密码未输入！
-        				 }elseif($pd ！== $user_password){
-                            $state = 4;  //密码错误！
-                         }elseif($pd == $user_password){
-        				 	$state = 5;  //账号密码匹配，登陆成功！
-        				 }
+                   foreach ($pd as $key => $value) {
+                       $a = $value;
+                   }
+        		if(!$user_password) {
+                    echo "!";
+        			$state = 3;  //密码未输入！
+        		}
+                else{
+                    if($a == $user_password){
+                        $state = 4;  
+                         //密码错误 ！
+                    }
+                    else{
+                       $state = 5;  //账号密码匹配，登陆成功
+                    }
+                }
         }
         $result['status'] = $state;
         echo json_encode($result);
