f4a3c71847081684fe1c83217e121205e2441180
zsyxydz <511639496@qq.com>
2017-04-17 19:03:46 +0800

    路径修改

--- Application/Home/Controller/RegisterController.class.php
+++ Application/Home/Controller/RegisterController.class.php
@@ -2,7 +2,7 @@
 namespace Home\Controller;
 use Think\Controller;
 class RegisterController extends Controller {
-    public function userregister(){
+    public function user_register(){
         $user = M('user')->select();
         // $this->assign();
         $this->display();
