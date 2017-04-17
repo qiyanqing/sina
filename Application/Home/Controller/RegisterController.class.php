09864d5192246a4ad16604762e19ea4b4edf7463
Your Name <you@example.com>
2017-04-17 19:15:16 +0800

    q

--- Application/Home/Controller/RegisterController.class.php
+++ Application/Home/Controller/RegisterController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class RegisterController extends Controller {
     public function user_register(){
-        $user = M('user')->select();
+        // $user = M('user')->select();
         // $this->assign();
         $this->display();
     }
